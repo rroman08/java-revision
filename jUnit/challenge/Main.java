cc8ad0d1a141b92e28d9de940f8e8fdb3baafbb5
Roman Rudolf <roman_rudolf@icloud.com>
2023-11-20 14:42:39 +0000

    Created challenge folder and Utilities class

---
+++ jUnit/challenge/Main.java
@@ -0,0 +1,9 @@
+package jUnit.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ jUnit/challenge/Utilities.java
@@ -0,0 +1,11 @@
+package jUnit.challenge;
+
+public class Utilities {
+
+    // Returns a char array containing every nth char. When
+    // sourceArray.length < n, returns sourceArray
+    public char[] everyNthChar(char[] sourceArray, int n) {
+
+        return null;
+    }
+}
