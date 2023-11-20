9f57828cf92ec38af46e3cabe596e41ecc18d087
RomanRudolf <roman_rudolf@icloud.com>
2023-11-20 12:17:03 +0000

    Set up a JUnit test class and configured the library, also created a Main class

---
+++ jUnit/BankAccountTest.java
@@ -0,0 +1,18 @@
+package jUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance() {
+    }
+}
---
+++ jUnit/Main.java
@@ -0,0 +1,8 @@
+package jUnit;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
