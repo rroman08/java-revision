a0435e3468c0b8c8de9c3b6b9b7d2b388fd6a0d3
RomanRudolf <roman_rudolf@icloud.com>
2023-11-20 12:44:30 +0000

    Created tests in BankAccountTest

--- jUnit/BankAccountTest.java
+++ jUnit/BankAccountTest.java
@@ -6,13 +6,28 @@ class BankAccountTest {
 
     @org.junit.jupiter.api.Test
     void deposit() {
+        BankAccount account = new BankAccount("Roman", "Rudolf", 1_000.00);
+        double balance = account.deposit(200.00, true);
+        // Uses the equals() method on the BankAccount class
+        assertEquals(1200.00, balance, 0);
     }
 
     @org.junit.jupiter.api.Test
     void withdraw() {
+        fail("This test has YET to be implemented");
     }
 
     @org.junit.jupiter.api.Test
-    void getBalance() {
+    void getBalance_deposit() {
+        BankAccount account = new BankAccount("Roman", "Rudolf", 1_000.00);
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance_withdraw() {
+        BankAccount account = new BankAccount("Roman", "Rudolf", 1_000.00);
+        account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
     }
 }
